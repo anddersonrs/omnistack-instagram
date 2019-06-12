8e8878818df6f505763dbc843638891c8cb692b9
Anderson Rodrigues <anddersonrds@gmail.com>
2019-06-12 17:26:19 -0300

    add Resize imgs and Socket Io Access

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+const server = require("http").server(app);
+const io = require("socket.io")(server);
 
 mongoose.connect(
   "mongodb+srv://admin_insta:admin@cluster0-n5uf1.mongodb.net/test?retryWrites=true&w=majority",
@@ -10,6 +15,19 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
 app.use(require("./routes"));
 
-app.listen(3333);
+server.listen(3333);
